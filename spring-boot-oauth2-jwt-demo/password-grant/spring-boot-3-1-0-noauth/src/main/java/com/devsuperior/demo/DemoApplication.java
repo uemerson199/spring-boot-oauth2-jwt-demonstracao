e16928c2b5cf2cdef2bd33a8103f73012cb722eb
uemerson199 <uemersonribeiro220@gmail.com>
2024-11-02 12:23:24 -0300

    BCrypt password

--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("Encode = " + passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("123456",
+				"$2a$10$fWlxHWjidei5b1jJ/WS.seT10/sN9sHKUHjoowf9dfVCAgtb7zDuq");
+
+		System.out.println(result);
+
+	}
 }
--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$fWlxHWjidei5b1jJ/WS.seT10/sN9sHKUHjoowf9dfVCAgtb7zDuq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$fWlxHWjidei5b1jJ/WS.seT10/sN9sHKUHjoowf9dfVCAgtb7zDuq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
